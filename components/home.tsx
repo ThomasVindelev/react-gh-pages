e499ce7e4d2d3f226d8caac50efb1c0abcab65fb
Thomas Vindelev <44167383+ThomasVindelev@users.noreply.github.com>
2022-12-19 23:04:59 +0100

    feat: home page

---
+++ components/home.tsx
@@ -0,0 +1,19 @@
+import { Grid, Typography } from "@mui/material"
+import React from "react"
+
+export const Home = () => {
+    return (
+        <Grid container display="flex" alignItems="center">
+            <Grid item>
+                <Typography variant="body2">
+                    asdf
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Typography variant="body2">
+                    ghjk
+                </Typography>
+            </Grid>
+        </Grid>
+    )
+}
