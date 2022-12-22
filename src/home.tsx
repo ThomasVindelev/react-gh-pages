15a40741be32e71f19d22e05f64e6e04a6078034
Thomas Vindelev <44167383+ThomasVindelev@users.noreply.github.com>
2022-12-22 18:55:18 +0100

    feat: added .tsconfig

--- src/home.tsx
+++ src/home.tsx
@@ -1,21 +1,21 @@
-import { Grid, Typography } from "@mui/material"
-import React from "react"
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
 
 const Home = () => {
-    return (
-        <Grid container display="flex" alignItems="center">
-            <Grid item>
-                <Typography variant="body2">
-                    asdf
-                </Typography>
-            </Grid>
-            <Grid item>
-                <Typography variant="body2">
-                    ghjk
-                </Typography>
-            </Grid>
-        </Grid>
-    )
-}
+  return (
+    <Grid
+      container
+      display='flex'
+      alignItems='center'
+    >
+      <Grid item>
+        <Typography variant='body2'>asdf</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant='body2'>ghjk</Typography>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Home;
